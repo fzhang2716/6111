de6f4e41028d57941854dbd7fa3ba27fe707b3fe
frank <fzhang0821@gmail.com>
2023-02-20 19:44:30 +0000

    complete install.sh

--- project1/install.sh
+++ project1/install.sh
@@ -1,5 +1,10 @@
-pip install --upgrade google-api-python-client
-pip install keybert
-pip install protobuf==3.20.*
-pip install spacy
-python -m spacy download en_core_web_sm
+sudo pip3 install --upgrade google-api-python-client
+pip3 install cython
+sudo apt-get install python3.7-dev
+pip3 install --upgrade setuptools wheel pip
+sudo pip3 install numpy
+sudo pip3 install --ignore-installed PyYAML
+sudo pip3 install keybert
+sudo pip3 install protobuf==3.20.*
+sudo pip3 install spacy
+python3 -m spacy download en_core_web_sm
