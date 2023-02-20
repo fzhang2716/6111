feae405f2424f8641048e27dbdca43ecd842a5d0
ruinengli <1390816524@qq.com>
2023-02-20 05:22:41 -0500

    final version

--- project1/test.py
+++ project1/test.py
@@ -3,6 +3,7 @@ from googleapiclient.discovery import build
 from keybert import KeyBERT
 import sys
 import spacy
+from copy import deepcopy
 
 ner_model = spacy.load('en_core_web_sm')
 keybert_model = KeyBERT()
@@ -23,49 +24,91 @@ def compute_word_scores(query_dict, contents):
 
     def get_keybert_score(content):
         
-        twograms_keywords = keybert_model.extract_keywords(content, keyphrase_ngram_range=(1,2), stop_words='english')
+        twograms_keywords = keybert_model.extract_keywords(content, keyphrase_ngram_range=(2,2), stop_words='english')
         onegram_keywords = keybert_model.extract_keywords(content, keyphrase_ngram_range=(1,1), stop_words='english')
-        scores = twograms_keywords + onegram_keywords
-        return scores
+        return twograms_keywords, onegram_keywords
         
     for content in contents:
-        scores = get_keybert_score(content)
-        for item in scores:
+        twograms_keywords, onegram_keywords = get_keybert_score(content)
+        occurred_words = set()
+        for item in twograms_keywords:
             key, score = item
             if key not in query_dict:
                 query_dict[key] = {}
                 query_dict[key]["importance"] = 0.
-                query_dict[key]["occurence"] = 0
+                query_dict[key]["occurrence"] = 0
+            word_1, word_2 = key.split()
+            if word_1 not in query_dict:
+                query_dict[word_1] = {}
+                query_dict[word_1]["importance"] = 0.
+                query_dict[word_1]["occurrence"] = 0
+            if word_2 not in query_dict:
+                query_dict[word_2] = {}
+                query_dict[word_2]["importance"] = 0.
+                query_dict[word_2]["occurrence"] = 0
+                
             query_dict[key]["importance"] += score
+            query_dict[word_1]["importance"] += score
+            query_dict[word_2]["importance"] += score
 
-    for item in query_dict.items():
-        key, value = item
-        query_dict[key]["occurence"] += 1
+            occurred_words.add(key)
+            occurred_words.add(word_1)
+            occurred_words.add(word_2)
+            
+        for item in onegram_keywords:
+            key, score = item
+            if key not in query_dict:
+                query_dict[key] = {}
+                query_dict[key]["importance"] = 0.
+                query_dict[key]["occurrence"] = 0
+            query_dict[key]["importance"] += score
+            query_dict[key]["occurrence"] += 1
+            
+            occurred_words.add(key)
+        
+    for key in occurred_words:
+        query_dict[key]["occurrence"] += 1
     
     return query_dict
 
-def fetch_new_words(query_dict, search_words):
+def fetch_new_words(_query_dict, _search_words):
     
+    previous_search_words = deepcopy(_search_words)
     new_word_count = 0
-    word_lst = sorted(query_dict.items(), key=lambda x: float(x[1]["importance"]/x[1]["occurence"]), reverse=True)
+    word_lst = sorted(_query_dict.items(), key=lambda x: float(0.25 * x[1]["importance"]+0.75*x[1]["occurrence"]), reverse=True)
     
     for item in word_lst:
         word, score = item
-        if word in search_words:
+        if word in _search_words:
             continue
         else:
-            search_words.append(word)
-            new_word_count += 1
-            if new_word_count >= 2:
-                break
+            if len(word.split()) == 1:
+                _search_words.append(word)
+                new_word_count += 1
+            else:
+                word_1, word_2 = word.split()
+                print(new_word_count)
+                _search_words.append(word_1)
+                new_word_count += 1
+                if new_word_count >= 2:
+                    break
+                else:
+                    _search_words.append(word_2)
+                    new_word_count += 1
+        if new_word_count >= 2:
+            break
     
-    return search_words
+    return _search_words
 
 def reorder_new_words(query_dict, search_words):
     
     new_words = {}
     for word in search_words:
-        new_words[word] = float(query_dict[word]["importance"]/query_dict[word]["occurence"])
+        try:
+            new_words[word] = float(0.25 * query_dict[word]["importance"]+ 0.75 * query_dict[word]["occurrence"])
+        except:
+            new_words[word] = 0.
+
     new_words_reordered = sorted(new_words.items(), key=lambda x: x[1], reverse=True)
     
     return [word[0] for word in new_words_reordered]
@@ -74,7 +117,6 @@ def main():
     
     api_key, engine_id, target_precision, search_words = processUserInput()
     # print(api_key, engine_id,precision_value,search_word)
-    searchResult = list()
 
 
     service = build(
@@ -85,37 +127,56 @@ def main():
     query_dict = {}
     
     search_words = search_words.split()
-
+    
+    iteration = 0
+    
     while precision < target_precision:
         
+        searchResult = list()
+
         data = service.cse().list(q=" ".join(search_words), cx=engine_id,).execute()
-        for i in data["items"]:
-            url = i["formattedUrl"]
-            title = i["title"]
-            description = i["snippet"]
+        
+        for page_idx, page in enumerate(data["items"]):
+            url = page["formattedUrl"]
+            title = page["title"]
+            description = page["snippet"].strip()
             full_text = title + ": " + description
             searchResult.append((url,title,description, full_text))
 
+        if len(searchResult) < 10:
+            print("please enter broad, ambiguous queries.")
+            break
+            
         user_feedback = []
-        for i in searchResult:
-            url, title, description, _ = i
+        for result in searchResult:
+            url, title, description, _ = result
             print((url,title,description))
-            rel = input(" relevent? type Y/N ")
-            if(rel == "Y" or rel == "y"):
+            rel = input(" relevent? type Y/N. ")
+            if rel in ["Y", "y"]:
                 user_feedback.append(1)
-            else:
+            elif rel in ["N", "n"]:
                 user_feedback.append(0)
+            else:
+                raise ValueError("Please type Y/N.")
 
-            relevant_pages = fetch_relevant_pages(user_feedback, searchResult)
-            query_dict = compute_word_scores(query_dict, relevant_pages)
-            search_words = fetch_new_words(query_dict, search_words)
-            search_words = reorder_new_words(query_dict, search_words)
+        if sum(user_feedback)/ 10. >= target_precision:
+            print("reach target precision at iter:{}.".format(iteration))
+            break
+        elif sum(user_feedback) == 0:
+            print("zero precision at iter:{}.".format(iteration))
+            break
             
-            
+        relevant_pages = fetch_relevant_pages(user_feedback, searchResult)
+        query_dict = compute_word_scores(query_dict, relevant_pages)
+        search_words = fetch_new_words(query_dict, search_words)
+        search_words = reorder_new_words(query_dict, search_words)
         
-
-
-
+        print("next search words:{}".format(search_words))
+        print(query_dict)
+        iteration += 1
+            
+            
+    
 if __name__ == "__main__":
 # python3 test.py AIzaSyDm0Wcp0OqqLiWypz0ijCRMjkHZ2mYisJs 2d6a0e6f605702952 0.1 steve jobs
 
