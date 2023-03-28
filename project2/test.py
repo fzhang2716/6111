9ed74038bafebbd668fd490e95d3ec578fadb4aa
ruinengli <1390816524@qq.com>
2023-03-28 11:36:45 -0400

    updated test.py

--- project2/test.py
+++ project2/test.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 import spacy
 from spanbert import SpanBERT
 import itertools
+import openai
 
 def get_text(url):
     text = ""
@@ -27,7 +28,7 @@ def process_text(model, text):
 
     processed_text = model(text)
     for sent in processed_text.sents:
-        named_entities_per_sent = sentence.ents
+        named_entities_per_sent = sent.ents
         sentence2entities.append({"sentence": sent, "named_entities": named_entities_per_sent})
 
     return sentence2entities
@@ -52,13 +53,13 @@ def process_required_pairs(sent2ents, required_relation_types, spacy2bert):
         sent, named_entities_per_sent = ents_per_sent["sentence"], ents_per_sent["named_entities"]
 
         required_ents = {}
-        pairs_per_sent = {}
+        pairs_per_sent = []
 
         for ent in named_entities_per_sent:
             name, label = ent.text, ent.label_
             if label not in required_ents:
                 required_ents[label] = []
-            required_ents[label].append(name)
+            required_ents[label].append(ent)
 
         for relation_type in required_relation_types:
             subj, obj = relation_type
@@ -66,12 +67,12 @@ def process_required_pairs(sent2ents, required_relation_types, spacy2bert):
                 continue
             subj_ents, obj_ents = required_ents[subj], required_ents[obj]
             relation_indexs = [(i, j) for i in range(len(subj_ents)) for j in range(len(obj_ents))]
-            pairs_per_sent[relation_type] = []
             for idx in relation_indexs:
                 subj_ent, obj_ent = subj_ents[idx[0]], obj_ents[idx[1]]
-                subj_info = (subj_ent.text, spacy2bert[subj_ent.label_], subj_ent.start, subj_ent.end)
-                obj_info = (obj_ent.text, spacy2bert[obj_ent.label_], obj_ent.start, obj_ent.end)
-                pairs_per_sent[relation_type].append({"tokens": sent, "subj": subj_info, "obj": obj_info})
+                subj_info = (subj_ent.text, spacy2bert[subj_ent.label_], (subj_ent.start, subj_ent.end-1))
+                obj_info = (obj_ent.text, spacy2bert[obj_ent.label_], (obj_ent.start, obj_ent.end-1))
+                tokens = [token.text for token in sent]
+                pairs_per_sent.append({"tokens": tokens, "subj": subj_info, "obj": obj_info})
 
         if len(pairs_per_sent) != 0:
             required_pairs.append(pairs_per_sent)
@@ -79,31 +80,31 @@ def process_required_pairs(sent2ents, required_relation_types, spacy2bert):
     
     return required_pairs, required_sents
 
-def process_relations(model, pairs, sents, confidence_threshold):
-    if isinstance(model, 'spanbert.SpanBERT'):
-        return extraction_spanbert(model, pairs, confidence_threshold)
+def process_relations(model, pairs, sents, confidence_threshold, required_relation_types_spanbert, required_relation_types, X, X_with_scores):
+    if isinstance(model, SpanBERT):
+        return extraction_spanbert(model, pairs, confidence_threshold, required_relation_types_spanbert, X, X_with_scores)
     elif isinstance(model, 'method'):
-        return extraction_gpt3(model, sents)
+        return extraction_gpt3(model, sents, required_relation_types, X)
     else:
         raise NotImplementedError
 
-def extraction_spanbert(spanbert_model, model, relations, t, required_relation_types):
+def extraction_spanbert(spanbert_model, relations, t, required_relation_types, required_tuples, required_tuples_with_scores):
 
-    required_tuples = set()
-    required_tuples_with_scores = {}
+
     for relations_per_sent in relations:
         relations_pred_per_sent = spanbert_model.predict(relations_per_sent)
+
         for relation, relation_pred in zip(relations_per_sent, relations_pred_per_sent):
             relation_type, confidence_score = relation_pred
             if relation_type in required_relation_types and confidence_score > t:
-                relation_tuple = (relation["subj"], relation["obj"])
+                relation_tuple = (relation["subj"][0], relation_type, relation["obj"][0])
+                # relation_tuple = (relation["subj"], relation["obj"])
                 required_tuples.add(relation_tuple)
                 if relation_tuple not in required_tuples_with_scores:
                     required_tuples_with_scores[relation_tuple] =  confidence_score
                 else:
                     previous_value = required_tuples_with_scores[relation_tuple]
                     required_tuples_with_scores[relation_tuple] = max(previous_value, confidence_score)
-        relation_preds.append(relation_pred_per_sent)
 
     return required_tuples, required_tuples_with_scores
 
@@ -113,16 +114,17 @@ def extraction_gpt3(gpt3_model, relations):
     for relations_per_sent in relations:
         sent = relation_per_sent["tokens"]
         response = gpt3_model(
-        model="text-davinci-003",
-        prompt="",
-        max_tokens=100,
-        temperature=0.1,
-        top_p=k,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-    response_text = response['choices'][0]['text']
-    sleep(5)
+            model="text-davinci-003",
+            prompt="",
+            max_tokens=100,
+            temperature=0.1,
+            top_p=k,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        response_text = response['choices'][0]['text']
+        sleep(5)
+        relation_preds.append(response_text)
     return response_text
 
 def processUserInput():
@@ -147,26 +149,50 @@ def main():
     bert2spacy = {"ORGANIZATION": "ORG", "PERSON": "PERSON", "LOCATION": "LOC", "CITY": "GPE", "COUNTRY": "GPE", "STATE_OR_PROVINCE": "GPE"}
     r2required_relations = {1: [("PERSON", "ORG")], 2: [("PERSON", "ORG")], 3: [("PERSON", "LOC"), ("PERSON", "GPE")], 4: [("ORG", "PERSON")]}
     r2required_entities = {1: ["PERSON", "ORG"], 2: ["PERSON", "ORG"], 3: ["PERSON", "LOC", "GPE"], 4: ["PERSON", "ORG"]}
+    r2relation_types_printed = {1: "Schools_Attended", 2: "Work_For", 3: "Live_In", 4: "Top_Member_Employees"}
+    r2relation_types = {1: "per:schools_attended", 2: "per:employee_of", 3: "per:cities_of_residence", 4: "org:top_members/employees"}
 
-    # openai settings
-    openai.api_key = ""
+    # settings
 
     extraction_method, api_key, engine_id, openai_key, r, t, q, k = processUserInput()
 
+    openai.api_key = openai_key
+    
     required_relation_types = r2required_relations[r]
     required_entity_types = r2required_entities[r]
+    required_spanbert_relations = r2relation_types[r]
+
+    model = spanbert_model if extraction_method == "-spanbert" else openai.Completion.create
     
-    #step 1
-    x = set()
-
-    search_results = list()
-
     service = build(
         "customsearch", "v1", developerKey=api_key
     )
 
+    used_queries, used_urls = set(), set()
+    X, X_with_scores = set(), {}
+
+    # write configurations
+    with open("transcript{}.txt".format(extraction_method), "w") as output_file:
+        output_file.write("Parameters:"+"\n")
+        output_file.write("Client key =\t {}\n".format(api_key))
+        output_file.write("Engine key =\t {}\n".format(engine_id))
+        output_file.write("OpenAI key =\t {}\n".format(openai_key))
+        output_file.write("Method =\t {}\n".format(extraction_method))
+        output_file.write("Relation =\t {}\n".format(r2relation_types_printed[r]))
+        output_file.write("Threshold = \t {}\n".format(t))
+        output_file.write("Query = \t {}\n".format(" ".join(q)))
+        output_file.write("Loading necessary libraries; This should take a minute or so ...)")
+
+    iter_idx = 0
+
     while True:
 
+        with open("transcript{}.txt".format(extraction_method), "a") as output_file: 
+            output_file.write("=========== Iteration: {} - Query: {} ===========".format(iter_idx, " ".join(q)) + "\n")
+
+        iter_idx += 1
+
+        search_results = []
         data = service.cse().list(q=" ".join(q), cx=engine_id,).execute()
 
         for item in data["items"]:
@@ -175,35 +201,37 @@ def main():
             description = item["snippet"]
             search_results.append((url,title,description))
 
-        # this dictionary use url as the key and the text from website as the value
-        plain_text_dict = {}
-        used_queries = set()
-
         for result in search_results:
-            curr_url = i[0]
-            if curr_url not in plain_text_dict:
-                webpage_text = get_text(i[0])
-                if webpage_text != None:
+            curr_url = result[0]
+            if curr_url not in used_urls:
+                used_urls.add(curr_url)
 
+                webpage_text = get_text(result[0])
+                if webpage_text != None:
                     sent2ents = process_text(spacy_model, webpage_text)
                     sent2ents_required = process_required_entities(sent2ents, required_entity_types)
+                    print("finished processing entities.")
                     pairs_required, sents_required = process_required_pairs(sent2ents_required, required_relation_types, spacy2bert)
-                    unique_tuples, unique_tuples_with_scores = process_relations(model, pairs_required, sents_required, t)
-
+                    print("finished processing pairs.")
+                    X, X_with_scores = process_relations(model, pairs_required, sents_required, t, required_spanbert_relations, required_relation_types, X, X_with_scores)
+                    print("finished processing relations.")
                     if extraction_method == "spanbert":
-                        unique_tuples_with_scores_ordered = sorted(unique_tuples_with_scores, key=lambda x:x[1], reverse=True)
+                        X_with_scores_ordered = sorted(X_with_scores, key=lambda x:x[1], reverse=True)
+                        assert len(X) == len(X_with_scores)
 
-                        if len(unique_tuples) > k:
-                            return unique_tuples_with_scores_ordered[:k]
+                        if len(X) > k:
+                            return [tuple_with_score[0] for tuple_with_score in X_with_scores_ordered][:k]
                         else:
-                            for unique_tuple in unique_tuples_with_scores_ordered:
-                                key, value = unique_tuple
-                                if key in used_queries:
+                            for unique_tuple in X_with_scores_ordered:
+                                relation_tuple, confidence_score = unique_tuple
+                                query = [relation_tuple[0], relation_tuples[2]]
+                                if query in used_queries:
                                     continue
                                 else:
-                                    used_queries.add(key)
-                                    q = key
+                                    used_queries.add(query)
+                                    q = query
                                     break
+
                     elif extraction_method == "gpt3":
                         if len(unique_tuples) > k:
                             return unique_tuples[:k]
@@ -217,12 +245,40 @@ def main():
                                     break
                     else:
                         raise NotImplementedError
-        # for i in plain_text_dict:
-        #     print(i)
-        #     print(plain_text_dict)
-        #     print("----------")
+
 
 if __name__ == "__main__":
 # python3 test.py -spanbert AIzaSyDm0Wcp0OqqLiWypz0ijCRMjkHZ2mYisJs 2d6a0e6f605702952 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 2 0.7 "bill gates microsoft" 10
 
     main()
+
+    # spacy_model = spacy.load("en_core_web_lg")
+    # spanbert_model = SpanBERT("./pretrained_spanbert")
+    
+    # spacy2bert = {"ORG": "ORGANIZATION", "PERSON": "PERSON", "GPE": "LOCATION", "LOC": "LOCATION"}
+    # bert2spacy = {"ORGANIZATION": "ORG", "PERSON": "PERSON", "LOCATION": "LOC", "CITY": "GPE", "COUNTRY": "GPE", "STATE_OR_PROVINCE": "GPE"}
+    # r2required_relations = {1: [("PERSON", "ORG")], 2: [("PERSON", "ORG")], 3: [("PERSON", "LOC"), ("PERSON", "GPE")], 4: [("ORG", "PERSON")]}
+    # r2required_entities = {1: ["PERSON", "ORG"], 2: ["PERSON", "ORG"], 3: ["PERSON", "LOC", "GPE"], 4: ["PERSON", "ORG"]}
+    # r2relation_types = {1: "per:schools_attended", 2: "per:employee_of", 3: "per:cities_of_residence", 4: "org:top_members/employees"}
+    # # openai settings
+
+    # extraction_method, api_key, engine_id, openai_key, r, t, q, k = processUserInput()
+
+    # openai.api_key = openai_key
+    
+    # required_relation_types = r2required_relations[r]
+    # required_entity_types = r2required_entities[r]
+    # required_spanbert_relations = r2relation_types[r]
+
+    # model = spanbert_model if extraction_method == "-spanbert" else openai.Completion.create
+
+    # webpage_text = "Bill Gates stepped down as chairman of Microsoft in February 2014 and assumed a new post as technology adviser to support the newly appointed CEO Satya Nadella."
+    
+    # sent2ents = process_text(spacy_model, webpage_text)
+    # sent2ents_required = process_required_entities(sent2ents, required_entity_types)
+    # pairs_required, sents_required = process_required_pairs(sent2ents_required, required_relation_types, spacy2bert)
+    # unique_tuples, unique_tuples_with_scores = process_relations(model, pairs_required, sents_required, t, required_spanbert_relations)
+    # print(unique_tuples)
+
+
+
